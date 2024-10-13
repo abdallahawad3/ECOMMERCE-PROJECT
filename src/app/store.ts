076fd8c30389201e78daa05e404398516244e91d
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-10-13 21:57:30 +0300

    Refactor store to persist cart state

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,13 +2,23 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import shoppingCartSlice from "./feature/cart/CartSlice";
 import drawerSlice from "./feature/global/globalSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+
+const persistedCart = persistReducer(persistConfig, shoppingCartSlice);
+
 export const store = configureStore({
   reducer: {
-    cart: shoppingCartSlice,
+    cart: persistedCart,
     drawer: drawerSlice,
   },
 });
 
+export const persister = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
